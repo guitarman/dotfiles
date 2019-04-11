cde6b8f2f3a8ccad67d6bb1b96dde789727e01ec
guitarman <guitarman@guitarman.sk>
2019-04-11 14:39:31 +0200

    use hdmi port for qhd display

--- home/.screenlayout/layout.sh
+++ home/.screenlayout/layout.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+xrandr --output HDMI-2 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output eDP-1 --off --output DP-2 --off
+
 sed -i 's/Xft\.dpi: 120/Xft\.dpi: 92/g' ~/.Xresources
 xrdb ~/.Xresources
 i3 restart
-
-xrandr --output HDMI-2 --mode 1920x1080 --pos 2560x176 --rotate normal --output HDMI-1 --off --output DP-1 --off --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
