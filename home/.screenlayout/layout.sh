21173f29c9810179324fc53885e35c334082f128
guitarman <guitarman@guitarman.sk>
2020-05-01 22:48:12 +0200

    Replace hardcoded scripts with ruby script that detecteds external display and switches to it, if possible, otherwise it keeps notebook monitor on

--- home/.screenlayout/layout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-2 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output eDP-1 --off --output DP-2 --off
-
-sed -i 's/Xft\.dpi: 120/Xft\.dpi: 92/g' ~/.Xresources
-xrdb ~/.Xresources
-i3 restart
